adbf8450d3947742a58ede7f591bfd521269f7a9
姜建世 <jianshi.jiang@mljr.com>
2017-05-18 12:01:44 +0800

    静态资源处理

--- src/main/java/com/hfq/house/manager/Application.java
+++ src/main/java/com/hfq/house/manager/Application.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.hfq.house.manager.interceptor.LoginInterceptor;
@@ -31,5 +32,11 @@ public class Application extends WebMvcConfigurerAdapter {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(loginInterceptor);
 		registry.addInterceptor(pageInterceptor);
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/stat/**").addResourceLocations("classpath:/stat/");
+		registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
 	}
 }
