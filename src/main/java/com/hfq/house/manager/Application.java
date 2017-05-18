1938bc74889aa3aef21338bc25e28f6babede18f
姜建世 <jianshi.jiang@mljr.com>
2017-05-18 14:43:43 +0800

    session超时时间

--- src/main/java/com/hfq/house/manager/Application.java
+++ src/main/java/com/hfq/house/manager/Application.java
@@ -4,6 +4,9 @@ import javax.annotation.Resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -39,4 +42,15 @@ public class Application extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/stat/**").addResourceLocations("classpath:/stat/");
 		registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
 	}
+	
+//	@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//		return new EmbeddedServletContainerCustomizer() {
+//			@Override
+//			public void customize(ConfigurableEmbeddedServletContainer container) {
+//				container.setSessionTimeout(600);//设置session失效时间为30分钟 单位为S
+//			}
+//		};
+//	}
+	
 }
--- src/main/java/com/hfq/house/manager/config/MyWebMvcConfig.java
+++ src/main/java/com/hfq/house/manager/config/MyWebMvcConfig.java
@@ -1,11 +1,12 @@
 package com.hfq.house.manager.config;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 
 /**
  * 
@@ -15,25 +16,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MyWebMvcConfig extends WebMvcConfigurerAdapter {
 
+	@Bean
+	public WebMvcConfigurerAdapter forwardToIndex() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addViewController("/").setViewName("forward:/index");
+			}
+		};
+	}
 
-    @Bean
-    public WebMvcConfigurerAdapter forwardToIndex() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                // forward requests to /admin and /user to their index.html
-//                registry.addViewController("/").setViewName(
-//                        "forward:/login.html");
-                registry.addViewController("/").setViewName(
-                        "forward:/index");
-            }
-        };
-    }
-    
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.setUseSuffixPatternMatch(false)
-                   .setUseTrailingSlashMatch(false);
-    }
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		configurer.setUseSuffixPatternMatch(false).setUseTrailingSlashMatch(false);
+	}
 
 }
