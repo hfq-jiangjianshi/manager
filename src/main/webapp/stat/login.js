27b2c121f372149f9ce3a2de816e48b9e61152ba
姜建世 <jianshi.jiang@mljr.com>
2017-05-18 10:15:25 +0800

    登录信息非空校验

--- src/main/webapp/stat/login.js
+++ src/main/webapp/stat/login.js
@@ -2,6 +2,14 @@ var opt = {
 	login : function() {
 		var account = $("#account").val();
 		var password = $("#password").val();
+		if (account.length == 0) {
+			$("#msg_top").text("用户名不能为空").css("color", "red");
+			return;
+		}
+		if (password.length == 0) {
+			$("#msg_top").text("密码不能为空").css("color", "red");
+			return;
+		}
 
 		$.ajax({
 			url : '/login',
@@ -12,7 +20,7 @@ var opt = {
 			},
 			dataType : 'json',
 			success : function(result) {
-				
+
 				if (result.status == 1) {
 					$("#msg_top").text(result.msg).css("color", "red");
 				} else {
